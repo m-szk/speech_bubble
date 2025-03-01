171050e1e861c74046da39e83bdea926783f8f35
m-szk <1091283+m-szk@users.noreply.github.com>
2025-03-01 16:26:25 +0900

    run_command追加

--- speech_bubble.py
+++ speech_bubble.py
@@ -25,53 +25,48 @@ def print_cmd(cmd):
     print(" ".join(output_cmd))
 
 
+def run_command(cmd):
+    print_cmd(cmd)
+    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+
 def create_text_image(text, font_name, font_size, output_image):
     # fmt: off
     cmd = [
         "magick", "-font", f"{font_name}",
         "-background", "None", "-pointsize", f"{font_size}",
-        f"label:{text}", f"{output_image}"]
+        f"label:{text}", f"{output_image}"
+    ]
     # fmt: on
-    print_cmd(cmd)
-    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    run_command(cmd)
 
 
 def create_background_image(
     width, height, nine_slice_image, left, top, right, bottom, output_image
 ):
-    # 画像のサイズを取得
     img_width, img_height = get_image_size(nine_slice_image)
     print(f"{nine_slice_image} image size: {img_width}x{img_height}")
 
     # fmt: off
     cmd = [
         "magick", nine_slice_image, "-write", "mpr:org", "+delete",
-        "(", "mpr:org", "-crop", f"{left}x{top}+0+0",
-        "+repage", ")",
-        "(", "mpr:org", "-crop", f"{right - left}x{top}+{left}+0",
-        "-resize", f"{width}x{top}!", ")",
-        "(", "mpr:org", "-crop", f"{img_width - right}x{top}+{right}+0",
-        "+repage", ")",
+        "(", "mpr:org", "-crop", f"{left}x{top}+0+0", "+repage", ")",
+        "(", "mpr:org", "-crop", f"{right - left}x{top}+{left}+0", "-resize", f"{width}x{top}!", ")",
+        "(", "mpr:org", "-crop", f"{img_width - right}x{top}+{right}+0", "+repage", ")",
         "+append", "-write", "mpr:top", "+delete",
-        "(", "mpr:org", "-crop", f"{left}x{bottom - top}+0+{top}",
-        "-resize", f"{left}x{height}!", ")",
-        "(", "mpr:org", "-crop", f"{right - left}x{bottom - top}+{left}+{top}",
-        "-resize", f"{width}x{height}!", ")",
-        "(", "mpr:org", "-crop", f"{img_width - right}x{bottom - top}+{right}+{top}",
-        "-resize", f"{img_width - right}x{height}!", ")",
+        "(", "mpr:org", "-crop", f"{left}x{bottom - top}+0+{top}", "-resize", f"{left}x{height}!", ")",
+        "(", "mpr:org", "-crop", f"{right - left}x{bottom - top}+{left}+{top}", "-resize", f"{width}x{height}!", ")",
+        "(", "mpr:org", "-crop", f"{img_width - right}x{bottom - top}+{right}+{top}", "-resize", f"{img_width - right}x{height}!", ")",
         "+append", "-write", "mpr:middle", "+delete",
-        "(", "mpr:org", "-crop", f"{left}x{img_height - bottom}+0+{bottom}",
-        "+repage", ")",
-        "(", "mpr:org", "-crop", f"{right - left}x{img_height - bottom}+{left}+{bottom}",
-        "-resize", f"{width}x{img_height - bottom}!", ")",
-        "(", "mpr:org", "-crop", f"{img_width - right}x{img_height - bottom}+{right}+{bottom}",
-        "+repage", ")",
+        "(", "mpr:org", "-crop", f"{left}x{img_height - bottom}+0+{bottom}", "+repage", ")",
+        "(", "mpr:org", "-crop", f"{right - left}x{img_height - bottom}+{left}+{bottom}", "-resize", f"{width}x{img_height - bottom}!", ")",
+        "(", "mpr:org", "-crop", f"{img_width - right}x{img_height - bottom}+{right}+{bottom}", "+repage", ")",
         "+append", "-write", "mpr:bottom", "+delete",
         "mpr:top", "mpr:middle", "mpr:bottom", "-append",
-        f"{output_image}"]
+        f"{output_image}"
+    ]
     # fmt: on
-    print_cmd(cmd)
-    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    run_command(cmd)
 
 
 def create_speech_bubble_image(background_image, output_image):
@@ -79,17 +74,15 @@ def create_speech_bubble_image(background_image, output_image):
     cmd = [
         "composite", "-gravity", "center",
         f"{TEMP_TEXT_PNG}", f"{background_image}",
-        f"{output_image}"]
+        f"{output_image}"
+    ]
     # fmt: on
-    print_cmd(cmd)
-    subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    run_command(cmd)
 
 
 def main():
-    # コマンドライン引数のパーサーを設定
-    parser = argparse.ArgumentParser(
-        description="Process text and 9slice image to speech bubble image."
-    )
+    # fmt: off
+    parser = argparse.ArgumentParser(description="Process text and 9slice image to speech bubble image.")
     parser.add_argument("text", type=str, help="speech bubble text")
     parser.add_argument("font_name", type=str, help="font name")
     parser.add_argument("font_size", type=str, help="font size")
@@ -106,15 +99,11 @@ def main():
 
     temp_image_name = get_temp_image_name(args.nine_slice_image)
     create_background_image(
-        text_img_width,
-        text_img_height,
-        args.nine_slice_image,
-        args.left,
-        args.top,
-        args.right,
-        args.bottom,
-        temp_image_name,
+        text_img_width, text_img_height,
+        args.nine_slice_image, args.left, args.top, args.right, args.bottom,
+        temp_image_name
     )
+    # fmt: on
 
     create_speech_bubble_image(temp_image_name, args.output_image)
 
